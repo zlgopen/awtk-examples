47d46dcca08f48a86d567ebbd00d24be6b872ec1
chentan <chentan@zlg.cn>
2019-11-06 09:39:07 +0800

    添加helloworld max sync 8c989ae2723d2a03b0472db36a789ef5c43663ae

---
+++ HelloWorld.Max-Demo/assets_gen.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python
+
+import os
+import sys
+import glob
+import shutil
+import platform
+import copy
+
+def joinPath(root, subdir):
+  return os.path.normpath(os.path.join(root, subdir))
+
+def removeDir(path):
+  if os.path.isdir(path):
+   print('rmdir:' + path);
+   shutil.rmtree(path)
+
+def toExePath(root, name):
+  if platform.system() == 'Windows':
+    return joinPath(root, name + '.exe')
+  else:
+    return joinPath(root, name)
+
+def forceMakeDirs(dst_dir):
+  removeDir(dst_dir)
+  os.makedirs(dst_dir)
+
+# tool.exe src_file dst_file option
+def generate(tools_dir, tool_name, src_dir, src_sub_dir, src_suffix, dst_dir, dst_sub_dir, dst_suffix, option, is_override):
+  tool_path = toExePath(tools_dir, tool_name)
+  src_dir = joinPath(src_dir, src_sub_dir)
+  dst_dir = joinPath(dst_dir, dst_sub_dir)
+
+  if not os.path.exists(tool_path) :
+    print(tool_path + ' not exist')
+    return
+  if not os.path.exists(src_dir) :
+    print(src_dir + ' not exist')
+    return
+  # Generate
+  if isinstance(src_suffix, list) :
+    for f in glob.glob(joinPath(src_dir, '*.*')):
+      raw=copy.copy(f);
+      if dst_suffix == '':
+        inc=''
+      else :
+        inc=copy.copy(f);
+        inc=inc.replace(src_dir, dst_dir)
+        for suffix in src_suffix : 
+          inc=inc.replace(suffix, dst_suffix)
+      if is_override:
+        if os.path.exists(inc):
+          os.remove(inc)
+      print(tool_path + ' ' + joinPath(src_dir, raw) + ' ' + joinPath(dst_dir, inc) + ' ' + option)
+      os.system(tool_path + ' ' + joinPath(src_dir, raw) + ' ' + joinPath(dst_dir, inc) + ' ' + option)
+  else :
+    for f in glob.glob(joinPath(src_dir, '*' + src_suffix)):
+      raw=copy.copy(f);
+      if dst_suffix == '':
+        inc=''
+      else :
+        inc=copy.copy(f);
+        inc=inc.replace(src_dir, dst_dir)
+        inc=inc.replace(src_suffix, dst_suffix)
+      if is_override:
+        if os.path.exists(inc):
+          os.remove(inc)
+      print(tool_path + ' ' + joinPath(src_dir, raw) + ' ' + joinPath(dst_dir, inc) + ' ' + option)
+      os.system(tool_path + ' ' + joinPath(src_dir, raw) + ' ' + joinPath(dst_dir, inc) + ' ' + option)
+
+# tool.exe src_file text_file dst_file font_size
+def genBmpFont(tools_dir, tool_name, src_dir, src_suffix, dst_dir, dst_suffix, sub_dir, font_size, is_remake_dir):
+  tool_path = toExePath(tools_dir, tool_name)
+  src_dir = joinPath(src_dir, sub_dir)
+  dst_dir = joinPath(dst_dir, sub_dir)
+
+  if not os.path.exists(tool_path) :
+    print(tool_path + ' not exist')
+    return
+  if not os.path.exists(src_dir) :
+    print(src_dir + ' not exist')
+    return
+  # Delete History
+  if is_remake_dir :
+    removeDir(dst_dir)
+    os.makedirs(dst_dir);
+  # Generate
+  text_file = joinPath(src_dir, 'text.txt')
+  for f in glob.glob(joinPath(src_dir, '*' + src_suffix)):
+    raw=copy.copy(f);
+    inc=copy.copy(f);
+    inc=inc.replace(src_dir, dst_dir)
+    if font_size == 18 :
+      inc=inc.replace(src_suffix, dst_suffix)
+    else :
+      inc=inc.replace(src_suffix, '_' + str(font_size) + dst_suffix)
+    if raw.find('.mini' + src_suffix) == -1 :
+      print(tool_path + ' ' + joinPath(src_dir, raw) + ' ' + text_file + ' ' + joinPath(dst_dir, inc) + ' ' + str(font_size))
+      os.system(tool_path + ' ' + joinPath(src_dir, raw) + ' ' + text_file + ' ' + joinPath(dst_dir, inc) + ' ' + str(font_size))
+
+def genTheme(tools_dir, src_dir, dst_dir):
+  forceMakeDirs(joinPath(dst_dir, 'styles'))
+  generate(tools_dir, 'themegen', src_dir, 'styles', '.xml', dst_dir, 'styles', '.data', '', 0)
+  generate(tools_dir, 'themegen', src_dir, 'styles', '.xml', src_dir, 'styles', '.bin', 'bin', 0)
+
+def genString(tools_dir, src_dir, dst_dir):
+  forceMakeDirs(joinPath(dst_dir, 'strings'))
+  generate(tools_dir, 'strgen', src_dir, 'strings', '.xml', dst_dir, 'strings', '', '', 0)
+  generate(tools_dir, 'strgen', src_dir, 'strings', '.xml', src_dir, 'strings', '', 'bin', 0)
+
+def genFont(tools_dir, src_dir, dst_dir):
+  forceMakeDirs(joinPath(dst_dir, 'fonts'))
+  generate(tools_dir, 'resgen', src_dir, 'fonts', '.ttf', dst_dir, 'fonts', '.res', '', 0)
+  # genBmpFont(tools_dir, 'fontgen', src_dir, '.ttf', dst_dir, '.data', 'fonts', 10, 0)
+  # genBmpFont(tools_dir, 'fontgen', src_dir, '.ttf', dst_dir, '.data', 'fonts', 18, 0)
+  # genBmpFont(tools_dir, 'fontgen', src_dir, '.ttf', dst_dir, '.data', 'fonts', 28, 0)
+  # genBmpFont(tools_dir, 'fontgen', src_dir, '.ttf', dst_dir, '.data', 'fonts', 32, 0)
+  # genBmpFont(tools_dir, 'fontgen', src_dir, '.ttf', dst_dir, '.data', 'fonts', 48, 0)
+  # genBmpFont(tools_dir, 'fontgen', src_dir, '.ttf', dst_dir, '.data', 'fonts', 64, 0)
+  # genBmpFont(tools_dir, 'fontgen', src_dir, '.ttf', dst_dir, '.data', 'fonts', 128, 0)
+
+def genImage(tools_dir, src_dir, dst_dir, dpi): 
+  IMAGEGEN_OPTIONS = '\"bgra|bgr565\"'
+  suffix = ['.png', '.jpg', '.bmp']
+
+  forceMakeDirs(joinPath(dst_dir, 'images'))
+  generate(tools_dir, 'resgen', src_dir, 'images/xx', suffix, dst_dir, 'images', '.res', '', 0)
+  generate(tools_dir, 'resgen', src_dir, 'images/' + dpi, suffix, dst_dir, 'images', '.res', '', 1)
+  #generate(tools_dir, 'imagegen', src_dir, 'images/' + dpi, suffix, dst_dir, 'images', '.data', IMAGEGEN_OPTIONS, 0)
+  
+def genData(tools_dir, src_dir, dst_dir): 
+  if os.path.exists(joinPath(src_dir, 'data')):
+    forceMakeDirs(joinPath(dst_dir, 'data'))
+    generate(tools_dir, 'resgen', src_dir, 'data', '.bin', dst_dir, 'data', '.res', '', 0)
+  
+def genUI(tools_dir, src_dir, dst_dir): 
+  forceMakeDirs(joinPath(dst_dir, 'ui'))
+  generate(tools_dir, 'xml_to_ui', src_dir, 'ui', '.xml', dst_dir, 'ui', '.data', '', 0)
+  generate(tools_dir, 'xml_to_ui', src_dir, 'ui', '.xml', src_dir, 'ui', '.bin', 'bin', 0)
+
+def check_python_version():
+  major_version = sys.version_info[0]
+  if major_version > 2:
+    # print("The python version is %d.%d. But python2.x is required.(Version 2.7 is well tested!)" %(major_version, sys.version_info[1]))
+    return True
+  return False
+
+def run():
+  if len(sys.argv) <= 3 :
+    print('Usage: assets_gen.py tools_dir src_dir dst_dir')
+    exit()
+
+  tools_dir = os.path.abspath(sys.argv[1])
+  src_dir = os.path.abspath(sys.argv[2])
+  dst_dir = os.path.abspath(sys.argv[3])
+
+  if len(sys.argv) > 4 :
+    opt = sys.argv[4]
+  else:
+    opt = ''
+  if not os.path.exists(tools_dir) :
+    print('tools dir not exist')
+    exit()
+  if not os.path.exists(src_dir) :
+    print('src dir not exist')
+    exit()
+
+  # Start Generate
+  if opt == '' or opt == '-style':
+    genTheme(tools_dir, src_dir, dst_dir)
+  if opt == '' or opt == '-string':
+    genString(tools_dir, src_dir, dst_dir)
+  if opt == '' or opt == '-font':
+    genFont(tools_dir, src_dir, dst_dir)
+  if opt == '' or opt == '-image':
+    if (len(sys.argv) > 5):
+      dpi = sys.argv[5]
+    else:
+      dpi = 'x1'
+    genImage(tools_dir, src_dir, dst_dir, dpi)
+  if opt == '' or opt == '-ui':
+    genUI(tools_dir, src_dir, dst_dir)
+  if opt == '' or opt == '-data':
+    genData(tools_dir, src_dir, dst_dir)
+
+run()
---
+++ HelloWorld.Max-Demo/src/window_main.c
@@ -0,0 +1,391 @@
+﻿/**
+ * File:   window_main.c
+ * Author: AWTK Develop Team
+ * Brief:  window main
+ *
+ * Copyright (c) 2018 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2019-10-24 Li Peihuang created
+ *
+ */
+#include "awtk.h"
+extern ret_t application_init(void);
+static void init_children_widget(widget_t* widget);
+
+#define PROP_BAR_TIMER "bar_timer_id"
+
+/**
+ * 进度条动画
+ */
+static ret_t progress_bar_animate_delta(widget_t* win, const char* name, int32_t delta) {
+  widget_t* progress_bar = widget_lookup(win, name, TRUE);
+  assert(progress_bar != NULL);
+
+  int32_t value = (PROGRESS_BAR(progress_bar)->value + delta);
+  value = tk_min(100, value);
+  value = tk_max(0, value);
+  widget_animate_value_to(progress_bar, value, 500);
+
+  return RET_OK;
+}
+
+/**
+ * 中英文互译
+ */
+static ret_t change_locale(const char* str) {
+  char country[3];
+  char language[3];
+
+  strncpy(language, str, 2);
+  strncpy(country, str + 3, 2);
+  locale_info_change(locale_info(), language, country);
+
+  return RET_OK;
+}
+
+/**
+ * 点击中英文互译按钮
+ */
+static ret_t on_language(void* ctx, event_t* e) {
+  (void)ctx;
+  (void)e;
+
+  const char* language = locale_info()->language;
+  if (tk_str_eq(language, "en")) {
+    change_locale("zh_CN");
+  } else {
+    change_locale("en_US");
+  }
+  return RET_OK;
+}
+
+/**
+ * 递增按钮事件
+ */
+static ret_t on_inc_click(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  (void)e;
+
+  progress_bar_animate_delta(win, "click_bar", 10);
+
+  return RET_OK;
+}
+
+/**
+ * 递减按钮事件
+ */
+static ret_t on_dec_click(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  (void)e;
+
+  progress_bar_animate_delta(win, "click_bar", -10);
+
+  return RET_OK;
+}
+
+/**
+ * 切换背景事件
+ */
+static ret_t on_blue_click(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  (void)e;
+
+  widget_use_style(win, "blue");
+
+  return RET_OK;
+}
+
+static ret_t on_green_click(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  (void)e;
+
+  widget_use_style(win, "green");
+
+  return RET_OK;
+}
+
+static ret_t on_yellow_click(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  (void)e;
+
+  widget_use_style(win, "yellow");
+
+  return RET_OK;
+}
+
+static ret_t on_default_click(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  (void)e;
+
+  widget_use_style(win, "default");
+
+  return RET_OK;
+}
+
+static ret_t on_pic_1_click(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  (void)e;
+
+  widget_use_style(win, "pic_1");
+
+  return RET_OK;
+}
+
+static ret_t on_pic_2_click(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  (void)e;
+
+  widget_use_style(win, "pic_2");
+
+  return RET_OK;
+}
+
+/**
+ * list_item点击事件
+ */
+static ret_t on_list_item_click(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  widget_t* list_item = WIDGET(e->target);
+  widget_t* item_label = widget_lookup_by_type(list_item, WIDGET_TYPE_LABEL, TRUE);
+  widget_t* label = widget_lookup(win, "list_view_label", TRUE);
+  value_t v;
+  return_value_if_fail(item_label != NULL, RET_OK);
+
+  value_set_wstr(&v, widget_get_text(item_label));
+  widget_set_text(label, value_wstr(&v));
+
+  return RET_OK;
+}
+
+static ret_t on_remove_self(void* ctx, event_t* e) {
+  widget_t* widget = WIDGET(e->target);
+  (void)ctx;
+
+  widget_remove_child(widget->parent, widget);
+  widget_destroy(widget);
+
+  return RET_OK;
+}
+
+static ret_t on_clone_self(void* ctx, event_t* e) {
+  widget_t* widget = WIDGET(e->target);
+  widget_t* clone = widget_clone(widget, widget->parent);
+  (void)ctx;
+
+  widget_on(clone, EVT_CLICK, on_clone_self, clone);
+
+  return RET_OK;
+}
+
+static ret_t on_slider_value_changing(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  widget_t* bar = widget_lookup(win, "changing_bar", TRUE);
+  widget_t* slider = WIDGET(e->target);
+  value_t v;
+
+  assert(bar != NULL);
+
+  value_set_int32(&v, widget_get_value(slider));
+  progress_bar_set_value(bar, value_int32(&v));
+
+  return RET_OK;
+}
+
+static ret_t on_slider_value_changed(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  widget_t* bar = widget_lookup(win, "changed_bar", TRUE);
+  widget_t* slider = WIDGET(e->target);
+  value_t v;
+
+  assert(bar != NULL);
+
+  value_set_int32(&v, widget_get_value(slider));
+  progress_bar_set_value(bar, value_int32(&v));
+
+  return RET_OK;
+}
+
+static ret_t on_bar_value_changed(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  widget_t* label = widget_lookup(win, "click_bar_label", TRUE);
+  widget_t* bar = WIDGET(e->target);
+  char text[32];
+  value_t v;
+
+  assert(bar != NULL);
+  assert(label != NULL);
+
+  value_set_int32(&v, widget_get_value(bar));
+  tk_snprintf(text, sizeof(text), "%d%%", value_int32(&v));
+  widget_set_text_utf8(label, text);
+
+  return RET_OK;
+}
+
+/**
+ * 定时器事件（定时增加progress_bar的值）
+ */
+static ret_t on_add_bar_value(const timer_info_t* timer) {
+  widget_t* win = WIDGET(timer->ctx);
+  widget_t* bar = widget_lookup(win, "click_bar", TRUE);
+  value_t v;
+  int32_t val;
+
+  assert(bar != NULL);
+
+  value_set_int32(&v, widget_get_value(bar));
+  val = value_int32(&v);
+  if (val >= 100) {
+    progress_bar_set_value(bar, 0);
+  }
+  progress_bar_animate_delta(win, "click_bar", 10);
+
+  return RET_REPEAT;
+}
+
+/**
+ * 正在编辑事件
+ */
+static ret_t on_changing(void* ctx, event_t* evt) {
+  widget_t* target = WIDGET(evt->target);
+  widget_t* win = WIDGET(ctx);
+  widget_t* label = widget_lookup(win, "edit_label", TRUE);
+
+  assert(label != NULL);
+
+  widget_set_text(label, target->text.str);
+
+  return RET_OK;
+}
+
+/**
+ * 关闭窗口
+ */
+static ret_t on_close(void* ctx, event_t* e) {
+  widget_t* win = WIDGET(ctx);
+  value_t v;
+  (void)e;
+
+  /*移除定时器*/
+  if (widget_get_prop(win, PROP_BAR_TIMER, &v) == RET_OK) {
+    timer_remove(value_uint32(&v));
+  }
+
+  return window_close(win);
+}
+
+/**
+ * 打开窗口
+ */
+static void open_window(const char* name, widget_t* to_close) {
+  widget_t* win = to_close ? window_open_and_close(name, to_close) : window_open(name);
+  uint32_t timer_id;
+  value_t v;
+  init_children_widget(win);
+
+  if (tk_str_eq(name, "basic")) {
+    /*添加定时器 */
+    timer_id = timer_add(on_add_bar_value, win, 1500);
+    value_set_uint32(&v, timer_id);
+    widget_set_prop(win, PROP_BAR_TIMER, &v);
+  }
+}
+
+static ret_t on_open_window(void* ctx, event_t* e) {
+  const char* name = (const char*)ctx;
+  (void)e;
+
+  open_window(name, NULL);
+
+  return RET_OK;
+}
+
+/**
+ * 子控件初始化(主要是设置click回调、初始显示信息)
+ */
+static ret_t init_widget(void* ctx, const void* iter) {
+  widget_t* widget = WIDGET(iter);
+  widget_t* win = widget_get_window(widget);
+  (void)ctx;
+
+  if (widget->name != NULL) {
+    const char* name = widget->name;
+
+    if (tk_str_eq(name, "language_btn")) {
+      widget_on(widget, EVT_CLICK, on_language, win);
+    } else if (tk_str_eq(name, "edit")) {
+      widget_on(widget, EVT_VALUE_CHANGING, on_changing, win);
+    } else if (tk_str_eq(name, "changing_slider")) {
+      widget_on(widget, EVT_VALUE_CHANGING, on_slider_value_changing, win);
+    } else if (tk_str_eq(name, "changed_slider")) {
+      widget_on(widget, EVT_VALUE_CHANGED, on_slider_value_changed, win);
+    } else if (tk_str_eq(name, "dec_btn")) {
+      widget_on(widget, EVT_CLICK, on_dec_click, win);
+    } else if (tk_str_eq(name, "inc_btn")) {
+      widget_on(widget, EVT_CLICK, on_inc_click, win);
+    } else if (tk_str_eq(name, "click_bar")) {
+      widget_on(widget, EVT_VALUE_CHANGED, on_bar_value_changed, win);
+    } else if (tk_str_eq(name, "blue_btn")) {
+      widget_on(widget, EVT_CLICK, on_blue_click, win);
+    } else if (tk_str_eq(name, "yellow_btn")) {
+      widget_on(widget, EVT_CLICK, on_yellow_click, win);
+    } else if (tk_str_eq(name, "green_btn")) {
+      widget_on(widget, EVT_CLICK, on_green_click, win);
+    } else if (tk_str_eq(name, "default_btn")) {
+      widget_on(widget, EVT_CLICK, on_default_click, win);
+    } else if (tk_str_eq(name, "pic_1_btn")) {
+      widget_on(widget, EVT_CLICK, on_pic_1_click, win);
+    } else if (tk_str_eq(name, "pic_2_btn")) {
+      widget_on(widget, EVT_CLICK, on_pic_2_click, win);
+    } else if (tk_str_eq(name, "clone_self")) {
+      widget_on(widget, EVT_CLICK, on_clone_self, win);
+    } else if (tk_str_eq(name, "remove_self")) {
+      widget_on(widget, EVT_CLICK, on_remove_self, win);
+    } else if (strstr(name, "open:") != NULL) {
+      widget_on(widget, EVT_CLICK, on_open_window, (void*)(name + 5));
+      widget_on(widget, EVT_LONG_PRESS, on_open_window, (void*)(name + 5));
+    } else if (tk_str_eq(name, "close")) {
+      widget_on(widget, EVT_CLICK, on_close, win);
+    }
+  }
+
+  const char* type = widget->vt->type;
+  if (tk_str_eq(type, WIDGET_TYPE_LIST_ITEM)) {
+    widget_on(widget, EVT_CLICK, on_list_item_click, win);
+  }
+
+  return RET_OK;
+}
+
+/**
+ * 初始化窗口上的子控件
+ */
+static void init_children_widget(widget_t* widget) {
+  widget_foreach(widget, init_widget, widget);
+}
+
+/**
+ * 初始化
+ */
+ret_t application_init() {
+  widget_t* win = window_open("main");
+  if (win) {
+    init_children_widget(win);
+
+#if 0
+    widget_t* widget = window_manager();
+    window_manager_set_show_fps(widget, TRUE);
+#endif
+  }
+  return RET_OK;
+}
